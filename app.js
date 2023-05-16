d9ccc5cd7d7b3084e549a881598f932bd170c9b7
alistoicakovacs <alistoicakovacs@icloud.com>
2023-05-16 22:47:50 +0200

    animation added for the crumbs - project closed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // gsap.fromTo(".text", { opacity: 0, y: 100 }, { opacity: 1, y: 0, duration: 1 });
 
+// Timeline
 const tl = gsap.timeline({
   defaults: { duration: 0.75, ease: Power1.easeOut },
 }); //here we can define how all the animations are going to behave
@@ -16,8 +17,17 @@ tl.fromTo(
 ); // once the first animation finishes, the second is starting right away
 tl.fromTo(".text", { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "<"); // the "<" is going to make the animation sync up with the previous animation
 
-const button = document.querySelector("button");
+// Cookie crumbs jump
+tl.fromTo(
+  ".cookie",
+  { y: 0, rotation: "0deg" },
+  { y: -20, rotation: "-20deg", yoyo: true, repeat: -1 }
+);
 
+tl.fromTo("#crumbs", { y: 0 }, { y: -20, yoyo: true, repeat: -1 }, "<");
+
+// Fading the cookie container out
+const button = document.querySelector("button");
 button.addEventListener("click", function () {
   gsap.to(".cookie-container", {
     opacity: 0,
